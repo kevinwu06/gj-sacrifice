612cb145a64eb96c77f8e96b571df68df4bfa6e4
MamboJiang <mambojiang@icloud.com>
2024-06-18 11:08:14 +0800

    添加关卡进入顺序逻辑，level1-5模板
    标题-1-2-3-4-5-标题，死了的话会在当前关卡复活

--- scripts/endpoint.gd
+++ scripts/endpoint.gd
@@ -3,4 +3,13 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if get_parent().name=="Map1":
 		Globals.go_to_world("res://scenes/title_screen.tscn")
-	
+	if get_parent().name=="level1map":
+		Globals.go_to_world("res://scenes/levels/level_2_game.tscn")
+	if get_parent().name=="level2map":
+		Globals.go_to_world("res://scenes/levels/level_3_game.tscn")
+	if get_parent().name=="level3map":
+		Globals.go_to_world("res://scenes/levels/level_4_game.tscn")
+	if get_parent().name=="level4map":
+		Globals.go_to_world("res://scenes/levels/level_5_game_ending.tscn")
+	if get_parent().name=="level5map":
+		Globals.go_to_world("res://scenes/title_screen.tscn")
